e9ca429796a9afad27704d6e6148ab3c0d72ca11
Pedro Cardoso da silva <pcsilvamaster@gmail.com>
2022-07-03 15:53:06 -0300

    Modification calculator

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,62 +1,78 @@
-const on = document.getElementById('on');
-const itens = document.getElementById('itens');
-var res1 = document.getElementById('valor1');
+'user strict';
 
+const res = document.getElementById('resultado');
+const number = document.querySelectorAll('[id*=tecla]');
+const operators = document.querySelectorAll('[id*= operador]');
 
-const addValue = (event) => {
-    console.log(event.target.id)
-    var operacao = ''
-    var val1 = 0
-    if(event.target.id == 1){
-        res1.value += event.target.id;
-    }else if(event.target.id == 2){
-        res1.value += event.target.id;
-    }else if(event.target.id == 3){
-        res1.value += event.target.id;
-    }else if(event.target.id == 4){
-        res1.value += event.target.id;
-    }else if(event.target.id == 5){
-        res1.value += event.target.id;
-    }else if(event.target.id == 6){
-        res1.value += event.target.id;
-    }else if(event.target.id == 7){
-        res1.value += event.target.id;
-    }else if(event.target.id == 8){
-        res1.value += event.target.id;
-    }else if(event.target.id == 9){
-        res1.value += event.target.id;
-    }else if(event.target.id == 0){
-        res1.value += event.target.id;
-    }else if(event.target.id == 'soma'){
-        operacao = 'soma'
-        val1 = res1.value
-        console.log(val1)
-        limpar()
-    }else if(event.target.id == 'diferenca'){
-        operacao = 'diferenca'
-        val1 = res1.value
-        limpar()
-    }else if(event.target.id == 'multiplicacao'){
-        operacao = 'multiplicacao'
-        val1 = res1.value
-        limpar()
-    }else if(event.target.id == 'divisao'){
-        operacao = 'divisao'
-        val1 = res1.value
-        limpar()
-    }else if(event.target.id == 'porcentagem'){
-        operacao = 'porcentagem'
-        val1 = res1.value
-        limpar()
-    }else if(event.target.id == 'igual'){
-        let val2 = res1.value
-        console.log(val2, val1)
-        console.log(operacao)
-    }
-    
-    const limpar = () => {
-        res1.value = ''
+let newNumber = true;
+let operatorValue;
+let NumberAfter;
+
+const pendentOperation = () => operatorValue =! undefined;
+
+const calc = () => {
+    const presentNumber = parseFloat(res.textContent);
+    newNumber = true;
+    resultado = eval(`${NumberAfter} ${operatorValue} ${presentNumber}`);
+    updateDisplay(resultado);
+}
+const updateDisplay = (texto) => {
+    if(newNumber){//if it is a new number it clears the field and displays
+        res.textContent = texto.toLocaleString('BR');
+        newNumber = false;
+    }else{//if not, it concatenates
+        res.textContent += texto;
     }
 }
-itens.addEventListener('click', addValue);
-on.addEventListener('click', limpar);
+const insertNumber = (event) => updateDisplay(event.target.textContent);
+number.forEach(numero => numero.addEventListener('click', insertNumber));
+
+const selectOperator = (event) => {
+    if(!newNumber){
+        newNumber = true;
+        operatorValue = event.target.textContent;//stored operator.
+        NumberAfter = parseFloat(res.textContent);//stored value in the textcontent display.
+    }
+}
+operators.forEach(operator => operator.addEventListener('click', selectOperator));
+
+const ativarIgual = () => {
+    calc()
+    operatorValue = undefined
+}
+document.getElementById('igual').addEventListener('click', ativarIgual);
+
+
+const clearCalc = () => {
+    operatorValue = undefined;
+    newNumber = true;
+    NumberAfter = undefined;
+}
+const clearDisplayAndCalc = () => {
+    res.textContent = '';
+    clearCalc();
+}
+document.getElementById('limpar').addEventListener('click', clearDisplayAndCalc);
+
+const removeEndCharacter = () => {
+    //slice e um metodo para array que te retorna um array de caracteres
+    res.textContent = res.textContent.slice(0, -1)
+    //Nesse caso ele vai remover o ultimo character
+}
+document.getElementById('backspace').addEventListener('click', removeEndCharacter);
+
+const thereIsDecimal = () => res.textContent.indexOf('.') == -1;
+const thereIsValue = () => res.textContent.length <= 0;
+const insertDecimal = () => {
+    //If I had done ao instead of the comma, in the calculate function I would have to make the exchange with replace
+    if(thereIsDecimal()){
+        if(thereIsValue()){
+            updateDisplay('0.')
+        }else{
+            updateDisplay('.')
+        }
+    }else{
+        this.alert('[ERRO]Não pode adicionar mais de um ponto!')
+    }
+}
+document.getElementById('decimal').addEventListener('click', insertDecimal);
